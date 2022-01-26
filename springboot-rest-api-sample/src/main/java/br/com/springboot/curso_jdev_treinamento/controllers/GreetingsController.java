30283e356a5f7a15e2e1f68505570dfff2641e0f
ricardoefamilia <ricardoefamilia@gmail.com>
2022-01-26 17:43:56 -0300

    Instalação do Postman - 26/01/22 - 17h40.

--- springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,10 +1,15 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,4 +46,14 @@ public class GreetingsController {
     	return "Mensagem: " + msg;
     }
     
+    
+    
+    /* métodos de API */
+    
+    @GetMapping(value = "listatodos")
+    @ResponseBody /*retorna os dados para o corpo da resposta */
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	List<Usuario> usuarios = usuarioRepository.findAll();
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /*retorna a lista em JSON*/
+    }
 }
