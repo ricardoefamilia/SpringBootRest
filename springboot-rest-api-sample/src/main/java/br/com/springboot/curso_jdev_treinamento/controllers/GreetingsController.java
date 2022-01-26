f2e790cf88ed2067f8454ef36af145045d99342d
ricardoefamilia <ricardoefamilia@gmail.com>
2022-01-26 15:50:51 -0300

    UsuarioRepository - 26/01/22 - 15h50.

--- springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,5 +1,6 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +8,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.springboot.curso_jdev_treinamento.model.Usuario;
+import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
+
 /**
  *
  * A sample greetings controller to return greeting text
  */
 @RestController /* mapeia todas as requisições */
 public class GreetingsController {
+	
+	@Autowired /* IC/CD ou CDI injeção de dependência  */
+	private UsuarioRepository usuarioRepository;
     /**
      *
      * @param name the name to greet
@@ -27,6 +34,10 @@ public class GreetingsController {
     @RequestMapping(value = "/mensagem/{msg}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String retornaMsg(@PathVariable String msg) {
+    	
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(msg);
+    	usuarioRepository.save(usuario);
     	return "Mensagem: " + msg;
     }
     
