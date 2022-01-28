aa214937221aa5f5921a983e7dc597a8df69b44e
ricardoefamilia <ricardoefamilia@gmail.com>
2022-01-28 17:21:35 -0300

    BuscarUserPorNome - 28/01/22 - 17h20.

--- springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -75,5 +76,30 @@ public class GreetingsController {
     	
     	usuarioRepository.deleteById(iduser);
     	return new ResponseEntity<String>("Excluído com sucesso!", HttpStatus.OK);
+    }
+    
+    @GetMapping(value = "buscauserporid")
+    @ResponseBody /* descrição da resposta */
+    public ResponseEntity<Usuario> buscauserporid(@RequestParam(name = "iduser") Long iduser){
+    	Usuario usuario = usuarioRepository.findById(iduser).get();
+    	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    }
+    
+    @PutMapping(value = "atualizar")
+    @ResponseBody /* descrição da resposta */
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario){
+    	if(usuario.getId() == null) {
+    		return new ResponseEntity<String>("Id não informado.", HttpStatus.OK);
+    	}
+    	
+    	Usuario user = usuarioRepository.saveAndFlush(usuario); /* salva e retorna os dados */
+    	return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+    }
+    
+    @GetMapping(value = "buscarUserPorNome")
+    @ResponseBody  /* descrição da resposta */
+    public ResponseEntity<List<Usuario>> buscarUserPorNome(@RequestParam(name = "nome") String nome){
+    	List<Usuario> usuarios = usuarioRepository.buscarUserPorNome(nome.trim().toUpperCase());
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
     }
 }
