bb9c02875f2ff6281239e2e76a0cbf14abe877cb
ricardoefamilia <ricardoefamilia@gmail.com>
2022-01-28 00:03:14 -0300

    Postman listartodos e salvar - 27/01/22 - 20h.

--- springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -55,5 +57,13 @@ public class GreetingsController {
     public ResponseEntity<List<Usuario>> listaUsuario(){
     	List<Usuario> usuarios = usuarioRepository.findAll();
     	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /*retorna a lista em JSON*/
+    }
+    
+    @PostMapping(value = "salvar")
+    @ResponseBody /* descrição da resposta */
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){ /* recebe os dados para salvar */
+    	
+    	Usuario user = usuarioRepository.save(usuario);
+    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
     }
 }
